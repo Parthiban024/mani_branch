fadf14f95cc9cc9d8bdf0fd4b3a5e918c5a76a0f
Parthiban024 <parthiban@objectways.com>
2023-12-19 10:27:57 +0530

    nbew

--- client/src/layouts/dashboard/index.js
+++ client/src/layouts/dashboard/index.js
@@ -1,318 +1,154 @@
-import Grid from "@mui/material/Grid";
+import React, { useState, useEffect } from 'react';
 import Card from "@mui/material/Card";
-import MDButton from "components/MDButton/index";
+import Grid from "@mui/material/Grid";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import CheckIcon from "@mui/icons-material/Check";
-import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import MDButton from "components/MDButton";
+import MDInput from "components/MDInput";
+import { DataGrid, GridToolbar, GridPagination } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import "react-datepicker/dist/react-datepicker.css";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
-import "react-datepicker/dist/react-datepicker.css";
 import { useSelector } from "react-redux";
-import React, { useState, useEffect } from "react";
+import axios from "axios";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 import moment from "moment";
-import { updateUserName, updateUserEmpId } from './userActions';
-import { useDispatch } from 'react-redux';
-import { DataGrid } from '@mui/x-data-grid';
-import DatePicker from 'react-datepicker';
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/Drawer";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import CloseIcon from "@mui/icons-material/Close";
+import IconButton from "@mui/material/IconButton";
+import { useSelect } from "@mui/base";
+import { ToastContainer, toast } from "react-toastify";
+import InputLabel from "@mui/material/InputLabel";
+import Pagination from '@mui/material/Pagination';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import 'layouts/Billing-Table/table.css'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import 'layouts/Billing-Table/table.css'
 
 
-function Attendance() {
-  const dispatch = useDispatch();
-  const [checkinTime, setCheckinTime] = useState(localStorage.getItem("checkinTime") || "");
-  const [checkoutTime, setCheckoutTime] = useState(localStorage.getItem("checkoutTime") || "");
-  const [checkinTimeForCheckout, setCheckinTimeForCheckout] = useState('');
-  const [total, setTotal] = useState(localStorage.getItem('total') || '');
-  const [remainingTime, setRemainingTime] = useState();
-  const name = useSelector((state) => state.auth.user.name);
-  const empId = useSelector((state) => state.auth.user.empId);
-  const [attendanceData, setAttendanceData] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(null);
 
-  useEffect(() => {
-    fetch(`/attendance/fetch/att-data?empId=${empId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const mappedData = data.map((item) => ({ ...item, id: item._id }));
-        setAttendanceData(mappedData);
-      })
-      .catch((error) => console.error('Error fetching data:', error));
-  }, [empId]);
+const UserDataUpload = () => {
 
-  const columns = [
-    { field: 'id', headerName: 'ID',  editable: false,
-   },
-    // { field: 'name', headerName: 'Name', width: 120 }, // Add this line
-    // { field: 'empId', headerName: 'Employee ID', width: 120 }, // Add this line
-    { field: 'checkInTime', headerName: 'Check In', width: 150,  flex: 1,  },
-    { field: 'checkOutTime', headerName: 'Check Out', width: 150,  flex: 1,  },
-    { field: 'total', headerName: 'Total', width: 150,  flex: 1,  },
-    {
-      field: 'currentDate',
-      headerName: 'Date',
-      width: 150,
-      valueGetter: (params) => moment(params.row.currentDate).format('YYYY-MM-DD'),
-    },
-  ];
 
-  const mappedData = attendanceData.map((item, index) => ({
-    ...item,
-    id: index + 1,
-    name: item.name,
-    empId: item.empId,
-  }));
-  // const filteredData = selectedDate
-  //   ? mappedData.filter((entry) => entry.currentDate.includes(selectedDate))
-  //   : mappedData;
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const handleCheckin = async () => {
-    const timeNow = moment().format('hh:mm a');
-    setCheckinTime(timeNow);
-    setCheckinTimeForCheckout(timeNow); // Store check-in time for later use in checkout
-
-    // Save data to local storage
-    localStorage.setItem("checkinTime", timeNow);
-    localStorage.setItem("name", name);
-    localStorage.setItem("empId", empId);
-
-    // Send data to the backend
-    // try {
-    //   const response = await fetch('/attendance/att', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ checkInTime: timeNow, name: name, empId: empId }),
-    //   });
-
-    //   if (response.ok) {
-    //     console.log('Attendance saved successfully');
-    //   } else {
-    //     console.error('Failed to save attendance');
-    //   }
-    // } catch (error) {
-    //   console.error('Error:', error);
-    // }
+  // Function to handle opening the drawer
+  const openDrawer = () => {
+    setDrawerOpen(true);
   };
 
-  const handleCheckout = async () => {
-    const checkTime = moment().format('hh:mm a');
-    setCheckoutTime(checkTime);
 
-    const checkinMoment = moment(checkinTimeForCheckout, 'hh:mm a'); // Use stored check-in time
-    const checkoutMoment = moment(checkTime, 'hh:mm a');
-    const overAll = moment.duration(checkoutMoment.diff(checkinMoment));
+  // Function to handle closing the drawer
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  }
 
-    // Update the state
-    setTotal(`${overAll.hours()}hrs : ${overAll.minutes()}mins`);
-
-    // Send data to the backend
-    localStorage.setItem("checkoutTime", checkTime);
-    localStorage.setItem("name", name);
-    localStorage.setItem("empId", empId);
-    localStorage.setItem('total', `${overAll.hours()}hrs : ${overAll.minutes()}mins`);
-
-    try {
-      const response = await fetch('/attendance/att', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          checkInTime: checkinTimeForCheckout, // Use stored check-in time
-          checkOutTime: checkTime,
-          name: name,
-          empId: empId,
-          total: `${overAll.hours()}hrs : ${overAll.minutes()}mins`,
-        }),
-      });
-
-      if (response.ok) {
-        console.log('Checkout time saved successfully');
-      } else {
-        console.error('Failed to save checkout time');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
-  const resetStateAndButtons = () => {
-    setCheckinTime('');
-    setCheckoutTime('');
-    setTotal('');
-    setRemainingTime('');
-    localStorage.removeItem('checkinTime');
-    localStorage.removeItem('checkoutTime');
-    localStorage.removeItem('total');
-  };
-
-  useEffect(() => {
-    const intervalId = setInterval(resetStateAndButtons, 60000);
-    return () => clearInterval(intervalId); // Cleanup on component unmount
-  }, []);
-
-  useEffect(() => {
-    const totalResetIntervalId = setInterval(() => {
-      localStorage.removeItem('total');
-      setTotal('');
-    }, 60000);
-
-    return () => clearInterval(totalResetIntervalId); // Cleanup on component unmount
-  }, []);
-
-  useEffect(() => {
-    const storedCheckinTime = localStorage.getItem("checkinTime");
-    const storedCheckoutTime = localStorage.getItem("checkoutTime");
-    const storedTotal = localStorage.getItem('total');
-    const storedName = localStorage.getItem("name");
-    const storedEmpId = localStorage.getItem("empId");
-
-    if (storedName) {
-      dispatch(updateUserName(storedName));
-
-      if (storedEmpId) {
-        dispatch(updateUserEmpId(storedEmpId));
-      }
-
-      if (storedTotal) {
-        setTotal(storedTotal);
-      }
-    }
-
-    if (storedCheckinTime) {
-      setCheckinTime(storedCheckinTime);
-    }
-
-    if (storedCheckoutTime) {
-      setCheckoutTime(storedCheckoutTime);
-    }
-  }, [dispatch]);
-
-  const calculateRemainingTime = () => {
-    const timeNow = moment();
-    const checkinMoment = moment(checkinTime, 'hh:mm a');
-    const checkoutMoment = moment(checkoutTime, 'hh:mm a');
-
-    const remainingDuration = checkoutMoment.diff(timeNow);
-    const remaining = moment.duration(remainingDuration);
-
-    setRemainingTime(`${remaining.hours()}hrs : ${remaining.minutes()}mins`);
-  };
-
-  useEffect(() => {
-    const remainingIntervalId = setInterval(calculateRemainingTime, 60000);
-    return () => clearInterval(remainingIntervalId);
-  }, [checkinTime, checkoutTime]);
 
   return (
-    <DashboardLayout>
-      <DashboardNavbar />
-      <Grid item xs={12} mt={1} >
-        <MDBox mt={4} mb={2}>
-          <Grid container spacing={3} justifyContent="center">
-            <Grid item xs={12} lg={8}>
-              <Card mb={3}>
-                <MDBox
-                  display="flex"
-                  flexDirection="column"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <MDTypography mt={2} mb={3} variant="caption" color="info" fontWeight="regular">
-                    <h1>Employee Attendance</h1>
-                  </MDTypography>
-                  <MDBox
-                    display="flex"
-                    width="850px"
-                    flexDirection="row"
-                    alignItems="center"
-                    justifyContent="space-evenly"
-                  >
-                    <Grid mt={3} item xs={12} md={6} lg={4}>
-                      <MDButton
-                        mb={3}
-                        type="submit"
-                        color="info"
-                        onClick={handleCheckin}
-                        disabled={!!checkinTime}
-                      >
-                        Check In!
-                      </MDButton>
-                      <MDBox display="flex" flexDirection="column">
-                        <MDTypography
-                          mt={3}
-                          variant="caption"
-                          color="dark"
-                          fontWeight="regular"
-                        >
-                          <h3>Time: {checkinTime}</h3>
-                        </MDTypography>
-                      </MDBox>
-                    </Grid>
-                    <Grid mt={3} item xs={12} md={6} lg={4}>
-                      <MDButton
-                        type="submit"
-                        color="success"
-                        onClick={handleCheckout}
-                        disabled={!!checkoutTime}
-                      >
-                        Check out!
-                      </MDButton>
-                      <MDBox display="flex" flexDirection="column">
-                        <MDTypography
-                          mt={3}
-                          variant="caption"
-                          color="dark"
-                          fontWeight="regular"
-                        >
-                          <h3>Time: {checkoutTime}</h3>
-                        </MDTypography>
-                      </MDBox>
-                    </Grid>
-                  </MDBox>
-                </MDBox>
-                <MDBox mt={4} px={10} display="flex" flexDirection="column">
-                  <MDTypography mb={1} variant="caption" color="dark" fontWeight="regular">
-                    <h3>Over All Time: {total}</h3>
-                  </MDTypography>
-                </MDBox>
-                <MDBox mt={4} px={10} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-                  <MDTypography mb={3} variant="caption" color="error" fontWeight="regular">
-                    <h1>🚧 Under Development Process 🚧</h1>
-                  </MDTypography>
-                </MDBox>
-              </Card>
-            </Grid>
-          </Grid>
+    <div>
+      <DashboardLayout>
+        <DashboardNavbar />
+        <div
+        style={{
+          display: "flex",
+          justifyContent: "end",
+        }}
+      >
+        <MDButton
+          variant="gradient"
+          color="success"
+          startIcon={<AddCircleOutlineIcon />}
+          onClick={openDrawer}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            // padding: "6px 12px", // Adjusted padding
+            fontSize: "0.7rem", // Adjusted font size
+            borderRadius: "10px",
+            textAlign: "center",
+            minHeight: "10px", // Adjust the height as needed
+            minWidth: "120px",
+           // Adjust the width as needed
+          }}
+        >
+          Add Employee
+        </MDButton>
+      </div>
+        <Drawer anchor="right" PaperProps={{ style: { width: 712 } }} open={drawerOpen} onClose={closeDrawer}>
+        <MDBox
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            mb: 2, // Adjusted margin-bottom
+          }}
+        >
+          <Typography variant="h6">New Task</Typography>
+          <IconButton
+            sx={{ position: "absolute", top: 10, right: 0 }} // Positioned to the top right corner
+            onClick={closeDrawer}
+          >
+            <CloseIcon />
+          </IconButton>
         </MDBox>
-      </Grid>
 
-      <Grid>
-      <Card>
-        {/* Date Picker for filtering */}
-        {/* <DatePicker
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
-          dateFormat="yyyy-MM-dd"
-          isClearable
-          placeholderText="Select Date"
-        /> */}
+        <MDBox pb={5} component="form" role="form" >
 
-        {/* DataGrid for displaying attendance data */}
-        
-        <div style={{ height: 370, width: '100%' }}>
-         
-          <DataGrid rows={mappedData} columns={columns} pageSize={5} />
-        
-        </div>
-        </Card>
-      </Grid>
+          <MDBox sx={{ width: 250, p: 2 }}>
+            <InputLabel htmlFor="hour">Daily Log</InputLabel>
 
-      <Footer />
-    </DashboardLayout>
+            <TextField
+              sx={{ width: 305, mt: 1 }}
+              select
+              fullWidth
+              id="hour"
+              name="sessionOne"
+              // value={value.sessionOne}
+              // onChange={handleInputchange}
+              variant="outlined"
+              InputLabelProps={{ shrink: true }}
+              SelectProps={{
+                native: true,
+              }}
+            >
+              <option value="">Select Hours</option>
+              <option value="1hour">1 Hour</option>
+              <option value="2hour">2 Hour</option>
+              <option value="3hour">3 Hour</option>
+              <option value="4hour">4 Hour</option>
+            </TextField>
+
+
+          </MDBox>
+          <MDBox
+            pt={3}
+            px={2}
+            display="flex"
+            justifyContent="end"
+            alignItems="center"
+          >
+            <MDButton type="submit" color="success">
+              Save
+            </MDButton>
+          </MDBox>
+        </MDBox>
+
+
+      </Drawer>
+      </DashboardLayout>
+    </div>
   );
-}
+};
 
-export default Attendance;
+export default UserDataUpload;
